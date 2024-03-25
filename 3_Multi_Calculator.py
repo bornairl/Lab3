8d1e8b7eadffcf117ee340c1342ee6354dce56f6
SergioMaceda <macsergg@gmail.com>
2024-03-25 11:39:43 -0400

    calculator

--- 3_Multi_Calculator.py
+++ 3_Multi_Calculator.py
@@ -1,22 +1,35 @@
 #Sergio Maceda
+#multi command calculator
+#
 
-#1 Input
-x1 = input("Enter x1: ")
-x2 = input("Enter x2: ")
-op = input("Enter operator: ")
+print("Calculator")
+
+sum = 0
+
+while True:
+    #1 Input
+    x1 = input("Enter x1: ")
+    x2 = input("Enter x2: ")
+    op = input("Enter operator: ")
+
+    #2 Process
+    if op == "+":
+        sum = int(x1) + int(x2)
+    elif op == "-":
+        sum = int(x1) - int(x2)
+    elif op == "*":
+        sum = int(x1) * int(x2)
+    elif op == "/":
+        sum = int(x1) / int(x2)
+    for x in range(int(x1),int(x2)+1):
+        sum += x
+
+    #3. Output
+    print(f"Sum: {sum}")
+    res = input("Continue? (Yes/No) :")
+    if res == "No":
+        print("Exit")
+        break;
 
 
-#2 Process
-
-if op == "+":
-    sum = int(x1) + int(x2)
-elif op == "-":
-    sum = int(x1) - int(x2)
-elif op == "*":
-    sum = int(x1) * int(x2)
-elif op == "/":
-    sum = int(x1) / int(x2)
-
-#3. Output
-
-print(f"sum: {sum}")
+    
